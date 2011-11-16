e7b2d3e88b46ff74825731b70122001e69f5a9cf
Jerome Etienne <jerome.etienne@gmail.com>
2011-11-16 19:45:06 +0100

    example improvement

--- fullscreenApi-polyfill.js
+++ fullscreenApi-polyfill.js
@@ -4,14 +4,16 @@
  * MIT license http://jetienne.mit-license.org/
 */
 
-if( 'webkitIsFullScreen' in document ){
-	Document.prototype.cancelFullScreen	= Document.prototype.webkitCancelFullScreen;
-	HTMLElement.prototype.requestFullScreen	= HTMLElement.prototype.webkitRequestFullScreen;
-	document.__defineGetter__("isFullScreen", function(){ return document.webkitIsFullScreen;	});
-	document.__defineGetter__("fullScreen"	, function(){ return document.webkitIsFullScreen;	});
-}else if( 'mozFullScreen' in document ){
-	Document.prototype.cancelFullScreen	= document.mozCancelFullScreen;
-	HTMLElement.prototype.requestFullScreen	= HTMLElement.prototype.mozRequestFullScreen;
-	document.__defineGetter__("isFullScreen", function(){ return document.mozFullScreen;	});
-	document.__defineGetter__("fullScreen"	, function(){ return document.mozFullScreen;	});
-}
+(function(){
+	if( 'webkitIsFullScreen' in document ){
+		Document.prototype.cancelFullScreen	= Document.prototype.webkitCancelFullScreen;
+		HTMLElement.prototype.requestFullScreen	= HTMLElement.prototype.webkitRequestFullScreen;
+		document.__defineGetter__("isFullScreen", function(){ return document.webkitIsFullScreen;	});
+		document.__defineGetter__("fullScreen"	, function(){ return document.webkitIsFullScreen;	});
+	}else if( 'mozFullScreen' in document ){
+		Document.prototype.cancelFullScreen	= document.mozCancelFullScreen;
+		HTMLElement.prototype.requestFullScreen	= HTMLElement.prototype.mozRequestFullScreen;
+		document.__defineGetter__("isFullScreen", function(){ return document.mozFullScreen;	});
+		document.__defineGetter__("fullScreen"	, function(){ return document.mozFullScreen;	});
+	}	
+}())
